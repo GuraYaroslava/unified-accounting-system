bcf28eab64ec9fe23b40d04560168f30df0ca1b2
GuraYaroslava <yaroslava_05@mail.ru>
2014-03-22 10:05:34 +1000

    Add a new function send requests (postRequest) in a separate module.

---
+++ static/js/utils.js
@@ -0,0 +1,25 @@
+define(["jquery"], function($) {
+
+    function postRequest(data, callback, url) {
+        $.ajax({
+            method: "post",
+            type: "post",
+            dataType: "json",
+            url: url,
+            data: JSON.stringify(data),
+            ContentType: "application/json; charset=utf-8",
+            success: function(data) {
+                console.log(data);
+                callback(data);
+            },
+            error: function(ajaxRequest, ajaxOptions, thrownError) {
+                console.log(thrownError);
+            }
+        });
+    };
+
+    return {
+        postRequest: postRequest
+    };
+
+});
