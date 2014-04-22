c705545543c1de21e82f3cdd03ee8cb35f48c28c
GuraYaroslava <yaroslava_05@mail.ru>
2014-04-22 22:54:38 +1000

    Fix sql documentation

--- sql/uas.sql
+++ sql/uas.sql
@@ -31,3 +31,55 @@ CREATE TABLE IF NOT EXISTS contests (
     name     varchar(128) NOT NULL UNIQUE,
     date     date         NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS blanks (
+    id         serial       NOT NULL PRIMARY KEY,
+    name       varchar(128) NOT NULL UNIQUE,
+    contest_id int          NOT NULL REFERENCES contests (id) ON DELETE CASCADE,
+    columns    varchar(32)[],
+    colNames   varchar(32)[],
+    types      varchar(32)[]
+);
+
+CREATE OR REPLACE FUNCTION del_elem_by_index(anyarray, integer)
+RETURNS anyarray AS
+    $BODY$ 
+        declare 
+            arr_ $1%type;
+            idx_ $2%type;
+        begin
+            for idx_ in array_lower($1, 1)..array_upper($1, 1) loop
+                if not idx_ = $2 then 
+                    arr_ = array_append(arr_, $1[idx_]);
+                else
+                    arr_ = array_append(arr_, NULL);
+                end if;
+            end loop;
+            return arr_;
+        end;
+    $BODY$
+    LANGUAGE plpgsql VOLATILE
+        COST 100;
+    ALTER FUNCTION del_elem_by_index(anyarray, integer)
+        OWNER TO admin;
+
+CREATE OR REPLACE FUNCTION del_null_from_arr(anyarray)
+RETURNS anyarray AS
+    $BODY$ 
+        declare
+            arr_ $1%type;
+            idx_ int;
+        begin
+            for idx_ in array_lower($1, 1)..array_upper($1, 1) loop
+                if not $1[idx_] = '' then
+                    arr_ = array_append(arr_, $1[idx_]);
+                end if;
+            end loop;
+            return arr_;
+        end;
+    $BODY$
+    LANGUAGE plpgsql VOLATILE
+        COST 100;
+    ALTER FUNCTION del_null_from_arr(anyarray)
+        OWNER TO admin;
+
